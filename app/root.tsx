63789c3e76e57b2c53b4dbe71a74d09ad1b9c7a1
thanapornko <thanapornkopatta@gmail.com>
2025-09-27 23:32:18 +0700

    fix: brand logo

--- app/root.tsx
+++ app/root.tsx
@@ -57,19 +57,10 @@ export const links: LinksFunction = () => [
     crossOrigin: 'anonymous',
   },
   { rel: 'dns-prefetch', href: 'https://res.cloudinary.com' },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon-32x32.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon-16x16.png',
-  },
-  { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+
+  { rel: 'icon', type: 'image/png', href: '/brandLogo.png' },
+  { rel: 'apple-touch-icon', href: '/brandLogo.png' },
+
   { rel: 'manifest', href: '/site.webmanifest' },
 ];
 
